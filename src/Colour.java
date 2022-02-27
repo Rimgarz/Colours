b53e320f939b94d57e2213d3e2143ccf99b45b2b
Louie Miller <louiemiller101@gmail.com>
2022-02-27 15:40:00 +0000

    yes

---
+++ src/Colour.java
@@ -0,0 +1,9 @@
+public class Colour {
+    public static void main(String[] args) throws Exception {
+        int x;
+        int y;
+    }
+    Colour O = new Colour();
+    O.x = 32;
+    O.y = 12;
+}
